970419d891f1cf6997d637672e9607c997042d09
hrishi2710 <hrishirules2710@gmail.com>
2019-08-29 10:36:34 +0530

    Added demo table for jdbc

---
+++ database-demo/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+create table person
+(
+	id integer not null,
+	name varchar(255) not null,
+	location varchar(255),
+	birth_date timestamp,
+	primary key(id)
+);
+
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10001, 'Hrishi', 'Patna', sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10002, 'Abhilash', 'Hyderabad', sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE)
+VALUES(10003, 'Charlie', 'Bangalore', sysdate());
